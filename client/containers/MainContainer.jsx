f06e11f6eef3d861469dc569f9be9dc1820a8947
Helen Regula <margaretregula@MacBook-Pro.local>
2021-04-24 13:55:56 -0400

    Finished Boilerplate

--- client/containers/MainContainer.jsx
+++ client/containers/MainContainer.jsx
@@ -5,5 +5,20 @@ import { connect } from 'react-redux';
 import * as actions from '../actions/actions.js';
 
 const mapStateToProps = ({
-    
+    //reducers: all the reducers
+}) => ({
+    //separate out each reducer
+});
+
+const mapDispatchToProps = dispatch => ({
+    //every action as a function with dispatch invoked
+    //eg: createNewContact: (<ANY PARAMETERS NEEDED>) => dipatch(actions.createNewContact(<PASS IN ARGUMENTS>))
 })
+
+const MainContainer = props => (
+    //insert all subsequent containers or buttons etc. here
+    //eg: <ContactsContainer {...props} />
+    <h2>HERE</h2>
+);
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainContainer);
