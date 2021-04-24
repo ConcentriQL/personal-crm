0d955546f232642271b6b941846f7dbf55d77cab
mikeFeldman15 <michaelfeldma1@gmail.com>
2021-04-24 09:07:13 -0400

    removing the query insert from our main server file

--- server/server.js
+++ server/server.js
@@ -7,12 +7,6 @@ const db = require('../models/dbModel.js')
 app.use('/build', express.static(path.join(__dirname, '../build')));
 
 app.get('/', (req, res) => {
-  const call = 'SELECT * FROM "public"."test" LIMIT 100';
-  db.query(call)
-    .then(pleasework => {
-      console.log(pleasework);
-    })
-    .catch(e => console.log('did not work in sql', e.stack))
   res.sendFile(path.resolve(__dirname, '../index.html'));
 })
 
