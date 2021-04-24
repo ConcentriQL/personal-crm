89d5fd7d7a52da41db9d2abce93aabf8a7b523ac
mikeFeldman15 <michaelfeldma1@gmail.com>
2021-04-24 08:59:26 -0400

    setting up the sql database and gitignores

--- server/server.js
+++ server/server.js
@@ -2,11 +2,18 @@ const express = require('express');
 const path = require('path');
 const PORT = 3000;
 const app = express();
+const db = require('../models/dbModel.js')
 
 app.use('/build', express.static(path.join(__dirname, '../build')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../index.html'));
+  const call = 'SELECT * FROM "public"."test" LIMIT 100';
+  db.query(call)
+    .then(pleasework => {
+      console.log(pleasework);
+    })
+    .catch(e => console.log('did not work in sql', e.stack))
+  res.sendFile(path.resolve(__dirname, '../index.html'));
 })
 
 app.listen(PORT);
