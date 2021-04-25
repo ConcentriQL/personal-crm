53c9dcafa565104d4b51a15d67336971e13f9c8e
mikeFeldman15 <michaelfeldma1@gmail.com>
2021-04-25 02:49:03 -0400

    same as the PR but with the latest server.js file

--- server/server.js
+++ server/server.js
@@ -4,13 +4,19 @@ const PORT = 3000;
 const app = express();
 const db = require('../models/dbModel.js');
 const apiroute = require('./routes/api.js');
+const dbroute = require('./routes/db.js')
 
+
+/*
+
+*/
 app.use('/build', express.static(path.join(__dirname, '../build')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../index.html'));
 })
 
+app.use('/api', apiroute);
 app.use('/api', apiroute);
 
 
